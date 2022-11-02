41ecba4eb1eae500b626184c30468e6535d9db27
Tijmen Baarda <tijmenbaarda@gmail.com>
2022-11-02 11:26:30 +0100

    Add optional static files directory argument

--- CLIStemmer.java
+++ CLIStemmer.java
@@ -7,7 +7,7 @@ import java.io.OutputStreamWriter;
 
 /**
  * @author Dafne van Kuppevelt
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
@@ -20,24 +20,32 @@ import java.io.OutputStreamWriter;
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    This file was modified by Tijmen Baarda (Utrecht Digital Humanities Lab)
     
  * Usage:
  * 
- * java CLIStemmer <inputfile> <outputfile> [<encoding>]
+ * Usage: java CLIStemmer <inputfile> <outputfile> [<encoding=UTF8>] [<staticfilesdir=StemmerFiles>]
  *
  */
 public class CLIStemmer {
 
 
 	public static void main(String[] args) {
-        if (args.length < 2) {
-            System.out.println("Usage: java CLIStemmer INPUTFILE OUTPUTFILE [ENCODING=UTF8]");
-            return;
-        }
-		StatsVector statsVector = new StatsVector();
+		if (args.length < 2) {
+			System.out.println("Usage: java CLIStemmer <inputfile> <outputfile> [<encoding=UTF8>] [<staticfilesdir=StemmerFiles>]");
+			return;
+		}
 		File fileIn = new File(args[0]);
 		File fileOut = new File(args[1]);
-		String encoding = args.length > 2 ? args[2] : "UTF-8"; 
+		String encoding = args.length > 2 ? args[2] : "UTF-8";
+		String staticPath = args.length > 3 ? args[3] : null;
+		StatsVector statsVector;
+		if (staticPath != null) {
+			statsVector = new StatsVector(new File(staticPath).getAbsolutePath());
+		} else {
+			statsVector = new StatsVector();
+		}
 		Stem stemmedText = new Stem ( fileIn, statsVector.getStaticFiles(), encoding);
 		System.out.println(stemmedText.displayText());
 		String[][] stemmedDocument = stemmedText.returnStatistics();
